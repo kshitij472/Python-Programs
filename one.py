7167148d4ded3726226fd2e1ed639992357f78ef
kshitij472 <chouhankshitij85@gmail.com>
2026-01-13 18:59:32 +0530

    removed 2 files and added to new folder core_logic

--- one.py
+++
@@ -1,9 +0,0 @@
-#Python Program to find the area of a triangle.
-
-base = float(input("enter length of the base of the triangle: "))
-height = float(input("enter a height of trainagle: "))
-
-area = 0.5 * base * height
-
-#display the result of the program
-print(f"Area of the Triangle is: {area}")
