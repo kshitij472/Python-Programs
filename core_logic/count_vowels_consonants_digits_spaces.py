68189220dc45d1714195434b6dbe713afd0e337c
kshitij472 <chouhankshitij85@gmail.com>
2026-01-14 20:14:46 +0530

    updated a program of counting vowels etc (added comments for better explanation)

--- core_logic/count_vowels_consonants_digits_spaces.py
+++ core_logic/count_vowels_consonants_digits_spaces.py
@@ -1,30 +1,30 @@
 #program for counting vowels, consonants, digits, spaces.
 
-text = input("enter string: ")
+text = input("enter string: ")             #for taking input from user
 
-vowels = 0
+vowels = 0                                #starts the counts from 0 
 consonants = 0
 digits = 0
 spaces = 0
 
-vowel_set = "aeiouAEIOU"
+vowel_set = "aeiouAEIOU"                  #created a list of vowels
 
-for ch in text:
+for ch in text:                           #created loop for every character
     
-    if ch == " ":
+    if ch == " ":                             #to check if the text has space in between
         spaces += 1
         
-    elif ch.isdigit():
+    elif ch.isdigit():                          #for digits 
         digits += 1
         
-    elif ch.isalpha():
+    elif ch.isalpha():                          #for alphabets
         
-        if ch in vowel_set:
+        if ch in vowel_set:                  # check whether the vowels are there or not
             vowels += 1
         else:
             consonants += 1
             
-print("Vowels: ",vowels)
+print("Vowels: ",vowels)                        #prints the final result 
 print("consonants: ", consonants)
 print("DIgits: ", digits)
 print("Spaces: ", spaces)
